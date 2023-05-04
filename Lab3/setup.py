02f7eb7995098902d666742e60c0a8fec0a2c658
Ilya Tsikhanionak <ilya.tihanyonok@yandex.by>
2023-05-04 23:57:38 +0300

    change setup file - add commits

--- Lab3/setup.py
+++ Lab3/setup.py
@@ -8,11 +8,9 @@ from os import path
 # The directory containing this file
 HERE = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# This call to setup() does all the work
 setup(
     name="SerizalizatorDeserializator",
     version="0.1.0",
